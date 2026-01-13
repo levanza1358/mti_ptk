bd3ccf6e8f06f58a404f458269eba1709087b905
Levanza <kiki.manda16@gmail.com>
2026-01-13 18:36:57 +0700

    Deploy web version to GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1704589148"
+    serviceWorkerVersion: "1179625161"
   }
 });
