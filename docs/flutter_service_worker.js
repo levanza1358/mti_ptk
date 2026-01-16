205951c9b04209faccb0343574a818ee0a9ea5df
levanza1358 <levanza1358@users.noreply.github.com>
2026-01-16 18:39:19 +0000

    Update docs for GitHub Pages [skip ci]

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "7c16154fdfd5ca98a95af4bdc48ce85f",
+"flutter_bootstrap.js": "cebd77eeeb20bb8e28a94cf9b2f62823",
 "version.json": "410a84dc68600254aac56fd272a3cfa0",
-"main.dart.js": "dd036992ba3007af572347050c0b5d65"};
+"main.dart.js": "a2aa359f4b365c694300159a89f7bba9"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
